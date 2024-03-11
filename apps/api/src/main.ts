6343d44c02015dc102ae6d1d83d80d069a0a1d88
Zang <bt.hoanggiang@gmail.com>
2024-03-12 02:18:38 +0700

    get HTML subject UITBOUY-37

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FetchSubjectData } from './subject/fetch-subject-data.cron';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    FetchSubjectData.fetch();
+    await app.listen(3000);
 }
 bootstrap();
