6e9808a0d201fc85715762d1ac47457a3e2bb1c4
Zang <bt.hoanggiang@gmail.com>
2024-03-12 06:57:29 +0700

    UITBOUY-36 fetch subject summary data

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FetchSubjectData } from './subject/fetch-subject-data.cron';
+import { FetchSubjectSummaryData } from './subject/fetch-subject-sumary-data.cron';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    FetchSubjectData.fetch();
+    FetchSubjectSummaryData.fetch();
     await app.listen(3000);
 }
 bootstrap();
