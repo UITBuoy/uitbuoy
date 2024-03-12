6a7192e323921c9f7e665747942219a7c4e354f8
Zang <bt.hoanggiang@gmail.com>
2024-03-12 07:23:34 +0700

    UITBOUY-38 create entity of subject

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,7 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { FetchSubjectData } from './subject/fetch-subject-data.cron';
-import { FetchSubjectSummaryData } from './subject/fetch-subject-sumary-data.cron';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
