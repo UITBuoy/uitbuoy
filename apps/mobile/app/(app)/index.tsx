e6d7662e0b3d2b4588fe8b0c7eee6b828204198b
Hoang Hy <hoanghy01122003@gmail.com>
2024-03-12 22:39:55 +0700

    config: configure nativewind

--- apps/mobile/app/(app)/index.tsx
+++ apps/mobile/app/(app)/index.tsx
@@ -1,25 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import Button from '../../components/Button/Button';
+import { StyleSheet, View } from 'react-native';
+import Button from '../../src/components/Button/Button';
 
 import { Text } from '@gluestack-ui/themed';
 
 export default function Page() {
     return (
-        <>
+        <View>
             <StatusBar style="auto" />
             <Text>This is home page</Text>
             <Button>Click me</Button>
-        </>
+        </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
--- apps/mobile/app/login.tsx
+++ apps/mobile/app/login.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function Page() {
-    return <Text>Login</Text>;
+    return (
+        <View className="">
+            <Text className="">Login</Text>
+        </View>
+    );
 }
