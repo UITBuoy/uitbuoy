4500315b3b10e74446164b23af00261cdd88b324
Zang <bt.hoanggiang@gmail.com>
2024-03-13 02:40:13 +0700

    refactor: add configuration to module (UITBOUY-41)

--- apps/api/src/subject/subject.module.ts
+++ apps/api/src/subject/subject.module.ts
@@ -4,9 +4,10 @@ import { SubjectResolver } from './subject.resolver';
 import { ApiModule } from 'src/api/api.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Subject } from './entities/subject.entity';
+import { SubjectConfiguration } from './configuration/subject.cofiguration';
 
 @Module({
     imports: [ApiModule, TypeOrmModule.forFeature([Subject])],
-    providers: [SubjectResolver, SubjectService],
+    providers: [SubjectResolver, SubjectService, SubjectConfiguration],
 })
 export class SubjectModule {}
