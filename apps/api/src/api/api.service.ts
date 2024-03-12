a39aee3e348cb791486f94f33a0650597a732d32
Zang <bt.hoanggiang@gmail.com>
2024-03-13 05:30:24 +0700

    refactor : get user profile

--- apps/api/src/api/api.service.ts
+++ apps/api/src/api/api.service.ts
@@ -1,31 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
-import WS_FUNCTION from 'src/common/constants/function-name';
 import API_URL from 'src/common/constants/url';
-import { User } from 'src/user/entities/user.entity';
-import { UserNotFoundException } from 'src/user/errors/not-found.error';
 import { MoodleException } from './errors/moodle.error';
 
 @Injectable()
 export class ApiService {
-    async getUserProfile({
-        token,
-        username,
-    }: {
-        token: string;
-        username: string;
-    }) {
-        const data = await this.fetchMoodleData<User[]>({
-            token,
-            functionName: WS_FUNCTION.GET_USER_PROFILE,
-            params: { field: 'username', 'values[0]': username },
-        });
-        if (data.length == 0) {
-            throw new UserNotFoundException(username);
-        }
-        return { ...data[0], token };
-    }
-
     async fetchMoodleData<T>({
         token,
         functionName,
---
+++ apps/api/src/api/services/user-api.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { ApiService } from '../api.service';
+import WS_FUNCTION from 'src/common/constants/function-name';
+import { UserNotFoundException } from 'src/user/errors/not-found.error';
+import { User } from 'src/user/entities/user.entity';
+
+@Injectable()
+export class UserAPIService {
+    constructor(private readonly apiService: ApiService) {}
+
+    async getUserProfile({
+        token,
+        username,
+    }: {
+        token: string;
+        username: string;
+    }) {
+        const data = await this.apiService.fetchMoodleData<User[]>({
+            token,
+            functionName: WS_FUNCTION.GET_USER_PROFILE,
+            params: { field: 'username', 'values[0]': username },
+        });
+        if (data.length == 0) {
+            throw new UserNotFoundException(username);
+        }
+        return { ...data[0], token };
+    }
+}
