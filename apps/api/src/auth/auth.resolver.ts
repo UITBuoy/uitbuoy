4aa4b96c9ae0190dc7c85d714ef027f5bf8bfaa1
Hoang Hy <hoanghy01122003@gmail.com>
2024-03-18 07:06:14 +0700

    feat: add logging module (UITBOUY-16)

--- apps/api/src/auth/auth.resolver.ts
+++ apps/api/src/auth/auth.resolver.ts
@@ -6,12 +6,14 @@ import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { AuthEntity } from './entities/auth.entity';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { LoggerService } from 'src/logger/logger.service';
 
 @Resolver(() => AuthEntity)
 export class AuthResolver {
     constructor(
         private readonly authService: AuthService,
         private readonly jwtService: JwtService,
+        private readonly loggerService: LoggerService,
     ) {}
 
     @Query(() => AuthEntity)
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.useGlobalInterceptors(new LoggingInterceptor());
 
     app.enableShutdownHooks();
 
