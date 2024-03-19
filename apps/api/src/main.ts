5a00cc1b701c27db3cd2310362ba2ee6ac17fca9
Hoang Hy <hoanghy01122003@gmail.com>
2024-03-19 12:30:22 +0700

    feat: add port log in api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 import { ConfigService } from '@nestjs/config';
+import { WinstonLogger } from './logger/winston.config';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -12,6 +13,9 @@ async function bootstrap() {
 
     const configService = app.get(ConfigService);
     const PORT = parseInt(configService.get<string>('APP_PORT'));
+
     await app.listen(PORT);
+
+    WinstonLogger.info(`Run server on port ${PORT}`);
 }
 bootstrap();
