a4018b0b75ba6d9edbcc62ccf316786aa939b148
Hoang Hy <hoanghy01122003@gmail.com>
2024-03-24 04:01:22 +0700

    feat(mobile): add basic UI for login (UITBUOY-24)

--- apps/mobile/app/login.tsx
+++ apps/mobile/app/login.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import TextField from '../src/components/TextField/TextField';
+import Button from '../src/components/Button/Button';
 
 export default function Page() {
     return (
-        <View className="">
-            <Text className="">Login</Text>
+        <View className=" flex flex-col w-full h-full">
+            <View
+                className=" w-full h-1/2 bg-[#039CCA]"
+                // style={{
+                //     fill: 'linear-gradient(153deg, #EDF1F5 1.34%, #039CCA 96.22%)',
+                // }}
+            ></View>
+            <View className=" pt-5 w-full h-fit px-5">
+                <TextField
+                    title="Username"
+                    type="text"
+                    placeholder="Nhập mã số sinh viên"
+                />
+                <TextField
+                    title="Password"
+                    type="password"
+                    placeholder="Nhập mật khẩu course"
+                />
+            </View>
+            <Button className="">Đăng nhập</Button>
         </View>
     );
 }
--- apps/mobile/src/components/Button/Button.tsx
+++ apps/mobile/src/components/Button/Button.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
 
 import { Button as _Button, ButtonText } from '@gluestack-ui/themed';
+import { View, TouchableOpacity, Text } from 'react-native';
 
-export default function Button({ children }: IProp) {
+export default function Button({ children, className }: IProp) {
     return (
-        <_Button
-            size="md"
-            variant="solid"
-            action="primary"
-            isDisabled={false}
-            isFocusVisible={false}
-        >
-            <ButtonText>{children}</ButtonText>
-        </_Button>
+        // <_Button
+        //     className=" bg-red-500 w-full"
+        //     size="md"
+        //     variant="solid"
+        //     action="primary"
+        //     isDisabled={false}
+        //     isFocusVisible={false}
+        // >
+        //     <ButtonText>{children}</ButtonText>
+        // </_Button>
+        <TouchableOpacity>
+            <View className=" bg-sky-500 rounded-lg flex mt-5 mx-5 p-3 items-center justify-center">
+                <Text className=" w-fit font-medium text-base text-white">
+                    {children}
+                </Text>
+            </View>
+        </TouchableOpacity>
     );
 }
 
 type IProp = {
+    className: string;
     children: React.ReactNode;
 };
