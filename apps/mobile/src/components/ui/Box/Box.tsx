acc05bb4509442629ea437ec993648fa40452ce0
Hoang Hy <hoanghy01122003@gmail.com>
2024-03-24 21:49:44 +0700

    feat(mobile): add ui of authentication page

---
+++ apps/mobile/src/components/ui/Box/Box.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { tva } from '@gluestack-ui/nativewind-utils/tva';
+import type { VariantProps } from '@gluestack-ui/nativewind-utils';
+
+const boxStyle = tva({});
+
+type IBoxProps = React.ComponentProps<typeof View> &
+    VariantProps<typeof boxStyle>;
+
+const Box = React.forwardRef(
+    ({ className, ...props }: IBoxProps, ref?: any) => {
+        return (
+            <View
+                ref={ref}
+                className={boxStyle({ class: className })}
+                {...props}
+            />
+        );
+    },
+);
+
+Box.displayName = 'Box';
+export { Box };
