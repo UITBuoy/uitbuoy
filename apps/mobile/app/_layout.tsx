54cb95818b3b50e4aa0eb2a1e7737995ecbf44bb
Hoang Hy <hoanghy01122003@gmail.com>
2024-03-24 18:03:52 +0700

    temp(mobile): config deps (UITBUOY-24)

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-import { config } from '@gluestack-ui/config';
-import { GluestackUIProvider } from '@gluestack-ui/themed';
+import { GluestackUIProvider } from '../src/components/gluestack-ui-provider/';
 import { Slot } from 'expo-router';
 
 export default function Layout() {
     return (
-        <GluestackUIProvider config={config}>
+        <GluestackUIProvider>
             <View style={styles.container}>
                 <Slot />
             </View>
