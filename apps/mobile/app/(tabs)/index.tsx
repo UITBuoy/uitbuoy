5a64f78705e04e632c779c753e92df3876f0b286
Hoang Hy <hoanghy01122003@gmail.com>
2024-03-26 00:33:30 +0700

    feat(mobile): add course search modal

--- apps/mobile/app/(tabs)/index.tsx
+++ apps/mobile/app/(tabs)/index.tsx
@@ -1,36 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { ScrollView, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { ScrollView, TouchableOpacity, View } from 'react-native';
 
-import { Text } from '@gluestack-ui/themed';
-import { useAuth } from '../../src/stores/auth.store';
-import { router } from 'expo-router';
-import { Button } from '../../src/components/ui/Button/Button';
+import { router, useRootNavigationState } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import DrawerIcon from '../../src/icons/drawer';
+import { useAuth } from '../../src/stores/auth.store';
+import MessageIcon from '../../src/icons/message';
+import NotificationIcon from '../../src/icons/notification';
+import SearchIcon from '../../src/icons/search';
+import TextField from '../../src/components/TextField/TextField';
 
 export default function Page() {
     const { isLogin, authLogout } = useAuth();
 
-    if (!isLogin) {
-        router.replace('/login');
-    }
+    const rootNavigationState = useRootNavigationState();
+
+    useEffect(() => {
+        if (!isLogin && rootNavigationState.key) {
+            router.replace('/login');
+        }
+    }, []);
 
     return (
         <View className=" flex-1 bg-white">
             <SafeAreaView>
                 <ScrollView className="">
-                    <Text className="">This is home page</Text>
+                    <View className=" px-4 py-2 flex flex-row">
+                        <TouchableOpacity className=" p-1 mr-auto">
+                            <DrawerIcon />
+                        </TouchableOpacity>
+                        <View className=" flex flex-row gap-6">
+                            <TouchableOpacity className=" p-1">
+                                <MessageIcon />
+                            </TouchableOpacity>
+                            <TouchableOpacity className=" p-1">
+                                <NotificationIcon />
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                    <TouchableOpacity
+                        onPress={() => {
+                            router.push('/modals/courseSearch');
+                        }}
+                    >
+                        <View className=" mt-4 mx-4 px-4 py-3 rounded-2xl bg-[#F2F2F2] flex flex-row gap-4 items-center">
+                            <SearchIcon />
+                            <TextField
+                                editable={false}
+                                fieldClassName=" text-sm"
+                                title="Search"
+                                type="text"
+                                placeholder="Search for courses..."
+                            />
+                        </View>
+                    </TouchableOpacity>
                 </ScrollView>
             </SafeAreaView>
-            {/* <StatusBar style="auto" />
-            <Text>This is home page</Text>
-            <Button
-                onPress={() => {
-                    authLogout();
-                }}
-            >
-                <Text>Signout</Text>
-            </Button> */}
         </View>
     );
 }
---
+++ apps/mobile/app/modals/courseSearch.tsx
@@ -0,0 +1,29 @@
+import { useRef } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import TextField from '../../src/components/TextField/TextField';
+
+export default function Modal() {
+    const searchRef = useRef(null);
+
+    return (
+        <View className=" flex-1 bg-white">
+            <SafeAreaView>
+                <View className="  mt-4 mx-4 flex flex-row items-center gap-2">
+                    <View className=" flex-1 px-4 py-3 rounded-2xl bg-[#F2F2F2] flex flex-row gap-4 items-center">
+                        <TextField
+                            ref={searchRef}
+                            fieldClassName=" text-sm"
+                            title="Search"
+                            type="text"
+                            placeholder="Search for courses..."
+                        />
+                    </View>
+                    <TouchableOpacity className=" flex-grow-0 px-4 py-2">
+                        <Text className=" text-neutral-40">Cancel</Text>
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
+        </View>
+    );
+}
