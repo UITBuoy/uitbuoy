46861afd1c00a62a03aa382109086a96a3a68fe1
Hoang Hy <hoanghy01122003@gmail.com>
2024-03-26 02:53:48 +0700

    feat(mobile): add basic item for course page

--- apps/mobile/app/(tabs)/courses.tsx
+++ apps/mobile/app/(tabs)/courses.tsx
@@ -1,3 +1,18 @@
+import { ScrollView, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import PageHeader from '../../src/components/PageHeader/PageHeader';
+import CourseSearch from '../../src/components/CourseSearch/CourseSearch';
+
 export default function Page() {
-    return <></>;
+    return (
+        <View className=" flex-1 bg-white">
+            <SafeAreaView>
+                <PageHeader />
+                <CourseSearch />
+                <ScrollView>
+                    <View className=" flex flex-col gap-10 pb-[100px]"></View>
+                </ScrollView>
+            </SafeAreaView>
+        </View>
+    );
 }
