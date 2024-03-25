a18b9427e2207a1c5db171b86ce96397270ea72d
Hoang Hy <hoanghy01122003@gmail.com>
2024-03-25 11:50:26 +0700

    feat(mobile): add login logo

--- apps/mobile/app/login.tsx
+++ apps/mobile/app/login.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
-import { Text, TextInput, View } from 'react-native';
+import { View } from 'react-native';
 import TextField from '../src/components/TextField/TextField';
 import { Button, ButtonText } from '../src/components/ui/Button/Button';
-import { Input, InputField } from '../src/components/ui/Input/Input';
-// import Button from '../src/components/Button/Button';
+import { Image } from 'expo-image';
+
+import LOGO from '../assets/app-logo.png';
 
 export default function Page() {
     return (
         <View className=" flex flex-col w-full h-full">
             <View
-                className=" w-full h-1/2 bg-[#039CCA]"
+                className=" w-full h-1/2 bg-[#039CCA] flex items-center"
                 // style={{
                 //     fill: 'linear-gradient(153deg, #EDF1F5 1.34%, #039CCA 96.22%)',
                 // }}
-            ></View>
-            <View className=" mt-8 flex flex-col gap-5 w-full h-fit px-5">
+            >
+                <Image
+                    style={{
+                        flex: 1,
+                        width: 300,
+                        backgroundColor: 'transparent',
+                    }}
+                    // className=" w-[300px] flex-1"
+                    source={LOGO}
+                    contentFit="contain"
+                    transition={1000}
+                />
+            </View>
+            <View className=" mt-5 flex flex-col gap-5 w-full h-fit px-5">
                 <TextField title="Username" type="text" placeholder="MSSV" />
                 <TextField
                     title="Password"
@@ -23,7 +36,7 @@ export default function Page() {
                 />
             </View>
             <Button
-                className=" bg-sky-500 h-fit p-3 mt-10 mx-5"
+                className=" bg-sky-500 h-fit p-3 mt-5 mx-5"
                 size="md"
                 variant="solid"
                 action="primary"
