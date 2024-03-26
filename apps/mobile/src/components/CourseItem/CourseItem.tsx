086edc3a1f91b9ea627ba87061eb89e077960f0f
Hoang Hy <hoanghy01122003@gmail.com>
2024-03-26 16:07:53 +0700

    feat(mobile): add course image to course list

--- apps/mobile/src/components/CourseItem/CourseItem.tsx
+++ apps/mobile/src/components/CourseItem/CourseItem.tsx
@@ -1,5 +1,5 @@
 import { Image } from 'expo-image';
-import { TouchableNativeFeedback, Text, View } from 'react-native';
+import { Text, TouchableNativeFeedback, View } from 'react-native';
 
 export default function CourseItem({
     fullname,
@@ -10,11 +10,13 @@ export default function CourseItem({
 }: CourseEntity) {
     return (
         <TouchableNativeFeedback>
-            <View className=" mx-4 border-[0.5px] rounded-2xl p-4 flex flex-row gap-2">
+            <View className=" mx-4 border-[0.5px] rounded-2xl p-4 flex flex-row gap-4">
                 <Image
                     style={{
-                        width: 50,
+                        width: 60,
+                        // height: 60,
                         backgroundColor: 'red',
+                        borderRadius: 8,
                     }}
                     source={{ uri: courseimage }}
                     contentFit="contain"
