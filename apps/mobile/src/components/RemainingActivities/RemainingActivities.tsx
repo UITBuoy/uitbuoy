27d82488ce068eb5508331d5e46eee0babba3167
Hoang Hy <hoanghy01122003@gmail.com>
2024-03-31 09:06:13 +0700

    fix(mobile): fix key not found

--- apps/mobile/src/components/RemainingActivities/RemainingActivities.tsx
+++ apps/mobile/src/components/RemainingActivities/RemainingActivities.tsx
@@ -13,41 +13,45 @@ export default function RemainingActivities() {
                 Remaining activities
             </Text>
             <View className=" py-5">
-                {data?.userEvents.map(({ activityname, timestart, course }) => (
-                    <TouchableNativeFeedback>
-                        <View className=" px-4 py-4 ">
-                            <View className="flex-1 flex flex-row justify-between items-center gap-4">
-                                <View className=" w-4/5 flex flex-col gap-1">
-                                    <Text className=" text-zinc-500 text-sm">
-                                        {new Intl.DateTimeFormat('en-GB', {
-                                            dateStyle: 'full',
-                                            timeStyle: 'short',
-                                            timeZone: 'Asia/Ho_Chi_Minh',
-                                        }).format(new Date(timestart * 1000))}
-                                    </Text>
-                                    <Text className=" text-sm text-zinc-500">
-                                        {course.fullname.split(' - ').at(0)}
-                                    </Text>
-                                    <Text className=" text-base font-medium">
-                                        {activityname}
-                                    </Text>
-                                </View>
-                                <View className=" flex flex-col items-center gap-1">
-                                    <Text
-                                        className={` w-12 h-12 pt-2 flex justify-center items-center rounded-full bg-primary-70 text-white text-center font-semibold text-xl`}
-                                    >
-                                        {moment(
-                                            new Date(timestart * 1000),
-                                        ).diff(moment(new Date()), 'days')}
-                                    </Text>
-                                    <Text className=" text-zinc-500">
-                                        days left
-                                    </Text>
+                {data?.userEvents.map(
+                    ({ id, activityname, timestart, course }) => (
+                        <TouchableNativeFeedback>
+                            <View className=" px-4 py-4 ">
+                                <View className="flex-1 flex flex-row justify-between items-center gap-4">
+                                    <View className=" w-4/5 flex flex-col gap-1">
+                                        <Text className=" text-zinc-500 text-sm">
+                                            {new Intl.DateTimeFormat('en-GB', {
+                                                dateStyle: 'full',
+                                                timeStyle: 'short',
+                                                timeZone: 'Asia/Ho_Chi_Minh',
+                                            }).format(
+                                                new Date(timestart * 1000),
+                                            )}
+                                        </Text>
+                                        <Text className=" text-sm text-zinc-500">
+                                            {course.fullname.split(' - ').at(0)}
+                                        </Text>
+                                        <Text className=" text-base font-medium">
+                                            {activityname}
+                                        </Text>
+                                    </View>
+                                    <View className=" flex flex-col items-center gap-1">
+                                        <Text
+                                            className={` w-12 h-12 pt-2 flex justify-center items-center rounded-full bg-primary-70 text-white text-center font-semibold text-xl`}
+                                        >
+                                            {moment(
+                                                new Date(timestart * 1000),
+                                            ).diff(moment(new Date()), 'days')}
+                                        </Text>
+                                        <Text className=" text-zinc-500">
+                                            days left
+                                        </Text>
+                                    </View>
                                 </View>
                             </View>
-                        </View>
-                    </TouchableNativeFeedback>
-                ))}
+                        </TouchableNativeFeedback>
+                    ),
+                )}
                 {data?.userEvents.length === 0 && !loading ? (
                     <Image
                         style={{
