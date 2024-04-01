9ed086dfdd36a8b9986fa9c9a7267ee2946eb750
Hoang Hy <hoanghy01122003@gmail.com>
2024-04-01 23:08:20 +0700

    feat(api): add description to course module entity

--- apps/api/src/course/resolvers/module.resolver.ts
+++ apps/api/src/course/resolvers/module.resolver.ts
@@ -1,13 +1,12 @@
 import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
-import { CourseSectionEntity } from '../entities/course-section.entity';
-import { CourseModuleEntity } from '../entities/course-module.entity';
 import { CourseContentEntity } from '../entities/course-content.entity';
+import { CourseModuleEntity } from '../entities/course-module.entity';
 
 @Resolver(() => CourseModuleEntity)
 export class ModuleResolver {
     constructor() {}
 
-    @ResolveField(() => [CourseContentEntity])
+    @ResolveField(() => [CourseContentEntity], { nullable: true })
     courseContents(@Parent() module: CourseModuleEntity) {
         return module.contents;
     }
