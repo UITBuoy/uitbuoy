2271f4ca5a1982b12bb0b3d0b717dc27f03c8149
Hoang Hy <hoanghy01122003@gmail.com>
2024-04-04 21:10:42 +0700

    fix(mobile): fix activity page file

--- apps/mobile/src/components/NativeButton/NativeButton.tsx
+++ apps/mobile/src/components/NativeButton/NativeButton.tsx
@@ -3,6 +3,7 @@ import { TouchableNativeFeedback, View } from 'react-native';
 export default function NativeButton({
     className,
     children,
+    onPress,
     borderRadius = 12,
 }: React.ComponentProps<typeof View> &
     React.ComponentProps<typeof TouchableNativeFeedback> & {
@@ -14,6 +15,7 @@ export default function NativeButton({
             style={{ borderRadius, overflow: 'hidden' }}
         >
             <TouchableNativeFeedback
+                onPress={onPress}
                 background={TouchableNativeFeedback.Ripple('#aaa', true)}
             >
                 {children}
