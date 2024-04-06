113f0f36c57eaae6eb6659a9985704f6550159cf
Hoang Hy <hoanghy01122003@gmail.com>
2024-04-06 23:08:01 +0700

    feat(api): add updating google user

--- apps/api/src/user/user.resolver.ts
+++ apps/api/src/user/user.resolver.ts
@@ -1,8 +1,23 @@
-import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CurrentUser } from '@/auth/decorators/current-user.decorator';
+import { JwtAuthGuard } from '@/auth/guards/jwt-auth.guard';
+import { UseGuards } from '@nestjs/common';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { CreateGoogleUserInput } from './dto/create-google-user.input';
+import { GoogleUser } from './entities/google-user.entity';
 import { User } from './entities/user.entity';
 import { UserService } from './user.service';
 
 @Resolver(() => User)
 export class UserResolver {
     constructor(private readonly userService: UserService) {}
+
+    @Mutation(() => GoogleUser)
+    @UseGuards(JwtAuthGuard)
+    addGoogleUser(
+        @CurrentUser() user: User,
+        @Args({ name: 'googleUser', type: () => CreateGoogleUserInput })
+        googleUser: CreateGoogleUserInput,
+    ) {
+        return this.userService.createGoogleUser(googleUser, user);
+    }
 }
--- apps/api/src/user/user.service.ts
+++ apps/api/src/user/user.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateGoogleUserInput } from './dto/create-google-user.input';
+import { GoogleUser } from './entities/google-user.entity';
 import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-    constructor(@InjectRepository(User) private repo: Repository<User>) {}
+    constructor(
+        @InjectRepository(User) private repo: Repository<User>,
+        @InjectRepository(GoogleUser)
+        private googleUserRepo: Repository<GoogleUser>,
+    ) {}
+
+    async createGoogleUser(googleUser: CreateGoogleUserInput, user: User) {
+        if (user) googleUser.user = user;
+        return this.googleUserRepo.save(googleUser);
+    }
 
     async create(user: User) {
         return this.repo.save(user);
