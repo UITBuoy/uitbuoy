aeaa5a0e6edc2d4a91c272e9a36581a2f0e19ab4
Hoang Hy <hoanghy01122003@gmail.com>
2024-04-08 03:22:05 +0700

    feat(api): add google task api service

---
+++ apps/api/src/api/services/google-task-api.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+import API_URL from 'src/common/constants/url';
+import { MoodleException } from '../errors/moodle.error';
+import { EventEntity } from '@/event/entities/event.entity';
+import { GoogleTaskException } from '../errors/google-task.error';
+
+@Injectable()
+export class GoogleTasksApiService {
+    private readonly BASE_URL = 'https://tasks.googleapis.com/tasks/v1';
+    private readonly TASK_LIST_TITLE = 'UITBUOY Events List';
+
+    async createTaskList({
+        accessToken,
+    }: {
+        accessToken: string;
+    }): Promise<any> {
+        const response = await axios.post(
+            `${this.BASE_URL}/users/@me/lists`,
+            {
+                title: this.TASK_LIST_TITLE,
+            },
+            { headers: { Authorization: `Bearer ${accessToken}` } },
+        );
+
+        const result = response.data;
+
+        if (response.status != 200) {
+            throw new GoogleTaskException();
+        }
+
+        return result;
+    }
+}
