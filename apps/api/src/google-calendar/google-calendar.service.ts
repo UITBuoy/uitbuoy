107729780afdc8ef6582df974ec9500b33f5ba0e
Hoang Hy <hoanghy01122003@gmail.com>
2024-04-08 04:24:07 +0700

    feat(api): update event sync

--- apps/api/src/google-calendar/google-calendar.service.ts
+++ apps/api/src/google-calendar/google-calendar.service.ts
@@ -5,12 +5,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { GoogleCalendarEventDto } from './dto/google-calendar-event.dto';
 import { GoogleCalendarEvent } from './entities/google-calendar-event.entity';
+import { GoogleTasksApiService } from '@/api/services/google-task-api.service';
 
 @Injectable()
 export class GoogleCalendarService {
     constructor(
         @InjectRepository(GoogleCalendarEvent)
         private readonly repo: Repository<GoogleCalendarEvent>,
+        private readonly googleTasksApiService: GoogleTasksApiService,
     ) {}
 
     async sync(
@@ -18,18 +20,54 @@ export class GoogleCalendarService {
         googleUser: GoogleUser,
         accessToken: string,
     ): Promise<GoogleCalendarEvent[]> {
-        const googleCalendarEvents: GoogleCalendarEventDto[] = events.map(
-            (event) => {
-                // TODO: call Google API to create new event
+        const googleCalendarEvents: GoogleCalendarEventDto[] =
+            await Promise.all(
+                events.map(async (event) => {
+                    const googleCalendarEvent = await this.repo.findOneBy({
+                        event,
+                        googleUser,
+                    });
 
-                return {
-                    id: '',
-                    lastSync: new Date().getTime(),
-                    event,
-                    googleUser,
-                };
-            },
-        );
+                    if (googleCalendarEvent) {
+                        const taskReponse =
+                            await this.googleTasksApiService.updateTask({
+                                accessToken,
+                                title: event.activityname,
+                                notes: event.description,
+                                due: new Date(
+                                    event.timeusermidnight * 1000,
+                                ).toISOString(),
+                                taskList: googleUser.taskListId,
+                                taskId: googleCalendarEvent.id,
+                            });
+
+                        return {
+                            id: taskReponse.id,
+                            lastSync: new Date().getTime(),
+                            event,
+                            googleUser,
+                        };
+                    }
+
+                    const taskReponse =
+                        await this.googleTasksApiService.createTask({
+                            accessToken,
+                            title: event.activityname,
+                            notes: event.description,
+                            due: new Date(
+                                event.timeusermidnight * 1000,
+                            ).toISOString(),
+                            taskList: googleUser.taskListId,
+                        });
+
+                    return {
+                        id: taskReponse.id,
+                        lastSync: new Date().getTime(),
+                        event,
+                        googleUser,
+                    };
+                }),
+            );
 
         return this.repo.save(googleCalendarEvents);
     }
