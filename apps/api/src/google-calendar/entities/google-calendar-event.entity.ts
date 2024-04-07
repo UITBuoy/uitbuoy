243af92c4a38d910e710ebacc30c346baa6388e2
Hoang Hy <hoanghy01122003@gmail.com>
2024-04-08 01:24:42 +0700

    feat(api): add google calendar event entity

---
+++ apps/api/src/google-calendar/entities/google-calendar-event.entity.ts
@@ -0,0 +1,31 @@
+import type { EventEntity } from '@/event/entities/event.entity';
+import type { GoogleUser } from '@/user/entities/google-user.entity';
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import {
+    Column,
+    Entity,
+    JoinColumn,
+    ManyToOne,
+    PrimaryColumn,
+    Relation,
+} from 'typeorm';
+
+@ObjectType()
+@Entity()
+export class GoogleCalendarEvent {
+    @Field()
+    @PrimaryColumn({ generated: true })
+    id: number;
+
+    @Field(() => Int)
+    @Column()
+    lastSync: number;
+
+    @ManyToOne('EventEntity')
+    @JoinColumn({ name: 'event_id' })
+    event: Relation<EventEntity>;
+
+    @ManyToOne('GoogleUser')
+    @JoinColumn({ name: 'google_user_id' })
+    googleUser: Relation<GoogleUser>;
+}
