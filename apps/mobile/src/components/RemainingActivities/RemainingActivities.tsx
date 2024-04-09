24a875055476848fa4004bd413371b7d2a6ce1e2
Hoang Hy <hoanghy01122003@gmail.com>
2024-04-09 10:00:43 +0700

    feat(mobile): redesign remaining activities

--- apps/mobile/src/components/RemainingActivities/RemainingActivities.tsx
+++ apps/mobile/src/components/RemainingActivities/RemainingActivities.tsx
@@ -30,10 +30,13 @@ export default function RemainingActivities() {
                                 });
                             }}
                         >
-                            <View className=" px-4 py-4 ">
+                            <View
+                                style={{ borderColor: '#CFCFCF' }}
+                                className=" px-4 py-4 border-[0.5px]"
+                            >
                                 <View className="flex-1 flex flex-row justify-between items-center gap-4">
                                     <View className=" w-4/5 flex flex-col gap-1">
-                                        <Text className=" text-zinc-500 text-sm">
+                                        {/* <Text className=" text-zinc-500 text-sm">
                                             {new Intl.DateTimeFormat('en-GB', {
                                                 dateStyle: 'full',
                                                 timeStyle: 'short',
@@ -41,23 +44,23 @@ export default function RemainingActivities() {
                                             }).format(
                                                 new Date(timestart * 1000),
                                             )}
-                                        </Text>
+                                        </Text> */}
                                         <Text className=" text-sm text-zinc-500">
                                             {course.fullname.split(' - ').at(0)}
                                         </Text>
-                                        <Text className=" text-base font-medium">
+                                        <Text className=" text-lg font-medium">
                                             {activityname}
                                         </Text>
                                     </View>
                                     <View className=" flex flex-col items-center gap-1">
                                         <Text
-                                            className={` w-12 h-12 pt-2 flex justify-center items-center rounded-full bg-primary-70 text-white text-center font-semibold text-xl`}
+                                            className={` w-12 h-12 pt-2 flex justify-center items-center rounded-full bg-primary-70 text-white text-center font-medium text-xl`}
                                         >
                                             {moment(
                                                 new Date(timestart * 1000),
                                             ).diff(moment(new Date()), 'days')}
                                         </Text>
-                                        <Text className=" text-zinc-500">
+                                        <Text className=" text-neutral-60 font-normal">
                                             days left
                                         </Text>
                                     </View>
