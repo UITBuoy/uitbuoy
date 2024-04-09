19899cc782476bcc1a0d39ed5adc3541d46c9397
Hoang Hy <hoanghy01122003@gmail.com>
2024-04-09 09:20:24 +0700

    feat(mobile): add course list skeleton

---
+++ apps/mobile/src/skeletons/CourseListSkeleton.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useRef } from 'react';
+import { Animated } from 'react-native';
+import { randomRange } from '../utils/random';
+
+export default function CourseListSkeleton() {
+    const opacityValue = useRef(new Animated.Value(0)).current;
+
+    const opacity = opacityValue.interpolate({
+        inputRange: [0, 0.2, 0.5, 0.8, 1],
+        outputRange: [0.2, 0.8, 1, 0.8, 0.2],
+    });
+
+    useEffect(() => {
+        Animated.loop(
+            Animated.timing(opacityValue, {
+                toValue: 1,
+                duration: 2000,
+                useNativeDriver: true,
+            }),
+        ).start();
+    }, []);
+
+    return Array(randomRange(3, 6))
+        .fill('')
+        .map((_, index) => (
+            <Animated.View
+                key={index}
+                style={{ opacity }}
+                className=" mx-4 flex flex-col gap-4 py-4 px-6 border-[0.5px] border-neutral-70 rounded-2xl"
+            >
+                <Animated.View
+                    style={{ opacity, width: randomRange(100, 300) }}
+                    className=" flex h-6 bg-neutral-90 rounded-lg"
+                ></Animated.View>
+                <Animated.View
+                    style={{ opacity, width: randomRange(50, 200) }}
+                    className=" flex h-6 bg-neutral-95 rounded-lg"
+                ></Animated.View>
+            </Animated.View>
+        ));
+}
---
+++ apps/mobile/src/utils/random.ts
@@ -0,0 +1,3 @@
+export function randomRange(start, end) {
+    return Math.floor(Math.random() * (end - start) + start);
+}
