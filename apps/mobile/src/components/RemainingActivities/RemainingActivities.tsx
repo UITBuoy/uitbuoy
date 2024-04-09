6231611068d3923705152c4ec4adf0085c31fa50
Hoang Hy <hoanghy01122003@gmail.com>
2024-04-09 12:34:42 +0700

    feat(mobile): add animation for home page

--- apps/mobile/src/components/RemainingActivities/RemainingActivities.tsx
+++ apps/mobile/src/components/RemainingActivities/RemainingActivities.tsx
@@ -4,6 +4,7 @@ import EMPTY_REMAINING_ACTIVITIES from '../../../assets/empty-remaining-activiti
 import { useUserEventsQuery } from '../../gql/graphql';
 import moment from 'moment';
 import { router } from 'expo-router';
+import Animated, { FadeInLeft, FadeOutLeft } from 'react-native-reanimated';
 
 export default function RemainingActivities() {
     const { data, loading, error } = useUserEventsQuery();
@@ -16,27 +17,32 @@ export default function RemainingActivities() {
             <View className=" py-5">
                 {data?.userEvents.map(
                     ({ id, activityname, timestart, course }, i) => (
-                        <TouchableNativeFeedback
+                        <Animated.View
                             key={id}
-                            onPress={() => {
-                                router.push({
-                                    pathname: '/modals/detail-activity',
-                                    params: {
-                                        id,
-                                        assignment_id:
-                                            data.userEvents[i].instance,
-                                        course_id: data.userEvents[i].course.id,
-                                    },
-                                });
-                            }}
+                            entering={FadeInLeft.delay((i + 1) * 100)}
+                            exiting={FadeOutLeft}
                         >
-                            <View
-                                style={{ borderColor: '#CFCFCF' }}
-                                className=" px-4 py-4 border-[0.5px]"
+                            <TouchableNativeFeedback
+                                onPress={() => {
+                                    router.push({
+                                        pathname: '/modals/detail-activity',
+                                        params: {
+                                            id,
+                                            assignment_id:
+                                                data.userEvents[i].instance,
+                                            course_id:
+                                                data.userEvents[i].course.id,
+                                        },
+                                    });
+                                }}
                             >
-                                <View className="flex-1 flex flex-row justify-between items-center gap-4">
-                                    <View className=" w-4/5 flex flex-col gap-1">
-                                        {/* <Text className=" text-zinc-500 text-sm">
+                                <View
+                                    style={{ borderColor: '#CFCFCF' }}
+                                    className=" px-4 py-4 border-[0.5px]"
+                                >
+                                    <View className="flex-1 flex flex-row justify-between items-center gap-4">
+                                        <View className=" w-4/5 flex flex-col gap-1">
+                                            {/* <Text className=" text-zinc-500 text-sm">
                                             {new Intl.DateTimeFormat('en-GB', {
                                                 dateStyle: 'full',
                                                 timeStyle: 'short',
@@ -45,28 +51,34 @@ export default function RemainingActivities() {
                                                 new Date(timestart * 1000),
                                             )}
                                         </Text> */}
-                                        <Text className=" text-sm text-zinc-500">
-                                            {course.fullname.split(' - ').at(0)}
-                                        </Text>
-                                        <Text className=" text-lg font-medium">
-                                            {activityname}
-                                        </Text>
-                                    </View>
-                                    <View className=" flex flex-col items-center gap-1">
-                                        <Text
-                                            className={` w-12 h-12 pt-2 flex justify-center items-center rounded-full bg-primary-70 text-white text-center font-medium text-xl`}
-                                        >
-                                            {moment(
-                                                new Date(timestart * 1000),
-                                            ).diff(moment(new Date()), 'days')}
-                                        </Text>
-                                        <Text className=" text-neutral-60 font-normal">
-                                            days left
-                                        </Text>
+                                            <Text className=" text-sm text-zinc-500">
+                                                {course.fullname
+                                                    .split(' - ')
+                                                    .at(0)}
+                                            </Text>
+                                            <Text className=" text-lg font-medium">
+                                                {activityname}
+                                            </Text>
+                                        </View>
+                                        <View className=" flex flex-col items-center gap-1">
+                                            <Text
+                                                className={` w-12 h-12 pt-2 flex justify-center items-center rounded-full bg-primary-70 text-white text-center font-medium text-xl`}
+                                            >
+                                                {moment(
+                                                    new Date(timestart * 1000),
+                                                ).diff(
+                                                    moment(new Date()),
+                                                    'days',
+                                                )}
+                                            </Text>
+                                            <Text className=" text-neutral-60 font-normal">
+                                                days left
+                                            </Text>
+                                        </View>
                                     </View>
                                 </View>
-                            </View>
-                        </TouchableNativeFeedback>
+                            </TouchableNativeFeedback>
+                        </Animated.View>
                     ),
                 )}
                 {data?.userEvents.length === 0 && !loading ? (
