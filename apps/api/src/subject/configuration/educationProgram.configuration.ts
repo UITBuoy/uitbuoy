039f8a44817d54db2228e1ade513677761b9bbe3
Zang <bt.hoanggiang@gmail.com>
2024-04-14 16:59:36 +0700

    feat: get other subject

--- apps/api/src/subject/configuration/educationProgram.configuration.ts
+++ apps/api/src/subject/configuration/educationProgram.configuration.ts
@@ -63,125 +63,143 @@ export class EducationProgramConfiguration implements OnApplicationBootstrap {
             'chuyên đề tốt nghiệp',
         ];
 
-        // //push courses
-        // for (let i = 0; i < courseLength; i++) {
-        //     const courseName = $educationProgram('div.accordion').eq(i).text();
-        //     courses.push({ course: courseName, majors: [] });
+        //push courses
+        for (let i = 0; i < courseLength; i++) {
+            const courseName = $educationProgram('div.accordion').eq(i).text();
+            courses.push({ course: courseName, majors: [] });
 
-        //     const majorLength: number = $educationProgram('div.acc-item')
-        //         .eq(i)
-        //         .children('div.panel')
-        //         .children('div.views-row').length;
+            const majorLength: number = $educationProgram('div.acc-item')
+                .eq(i)
+                .children('div.panel')
+                .children('div.views-row').length;
 
-        //     for (let j = 0; j < majorLength; j++) {
-        //         const majorName = $educationProgram('div.panel')
-        //             .eq(i)
-        //             .children('div')
-        //             .eq(j)
-        //             .children('a')
-        //             .text();
-        //         const link: string = $educationProgram('div.panel')
-        //             .eq(i)
-        //             .children('div')
-        //             .eq(j)
-        //             .children('a')
-        //             .attr('href');
-        //         courses[i].majors.push({
-        //             majorName,
-        //             link,
-        //             totalCredit: '',
-        //             deepMajor: [],
-        //             subjects: [],
-        //         });
-        //     }
-        //     //get total credit
-        //     for (let j = 0; j < majorLength; j++) {
-        //         if (
-        //             courses[0] &&
-        //             courses[0].majors[j] &&
-        //             courses[0].majors[j].link
-        //         ) {
-        //             const $fieldRoot = await getPayload(
-        //                 `https://daa.uit.edu.vn${courses[0].majors[j].link}`,
-        //             );
-        //             const table = $fieldRoot('dd:nth-child(6)').html(); //gia tri nay truyen vao
-        //             const $getTotalCredit = cheerio.load(table);
-        //             courses[0].majors[j].totalCredit = $getTotalCredit('table')
-        //                 .eq(0)
-        //                 .contents()
-        //                 .children('tr')
-        //                 .last()
-        //                 .children('td')
-        //                 .last()
-        //                 .prev()
-        //                 .text()
-        //                 .trim();
-        //         }
-        //     }
+            for (let j = 0; j < majorLength; j++) {
+                const majorName = $educationProgram('div.panel')
+                    .eq(i)
+                    .children('div')
+                    .eq(j)
+                    .children('a')
+                    .text();
+                const link: string = $educationProgram('div.panel')
+                    .eq(i)
+                    .children('div')
+                    .eq(j)
+                    .children('a')
+                    .attr('href');
+                courses[i].majors.push({
+                    majorName,
+                    link,
+                    totalCredit: '',
+                    deepMajor: [],
+                    subjects: [],
+                });
+            }
+            //get total credit
+            for (let j = 0; j < majorLength; j++) {
+                if (
+                    courses[0] &&
+                    courses[0].majors[j] &&
+                    courses[0].majors[j].link
+                ) {
+                    const $fieldRoot = await getPayload(
+                        `https://daa.uit.edu.vn${courses[0].majors[j].link}`,
+                    );
+                    const table = $fieldRoot('dd:nth-child(6)').html(); //gia tri nay truyen vao
+                    const $getTotalCredit = cheerio.load(table);
+                    courses[0].majors[j].totalCredit = $getTotalCredit('table')
+                        .eq(0)
+                        .contents()
+                        .children('tr')
+                        .last()
+                        .children('td')
+                        .last()
+                        .prev()
+                        .text()
+                        .trim();
+                }
+            }
+        }
 
-        //     // for (let j = 0; j < majorLength; j++) {
-
-        //     // }
-        // }
-
-        // //get subjects by type (dai cuong)
-        // const tableLength = $data('table').eq(1).find('tr').length;
-        // for (let j = 0; j < courses[0].majors.length; j++) {
-        //     for (let i = 1; i < tableLength; i++) {
-        //         let textIndex = $data('table')
-        //             .eq(1)
-        //             .find('tr')
-        //             .eq(i)
-        //             .find('td')
-        //             .eq(0)
-        //             .text()
-        //             .trim();
-        //         const columnContent = $data('table')
-        //             .eq(1)
-        //             .find('tr')
-        //             .eq(i)
-        //             .find('td')
-        //             .eq(0)
-        //             .text();
-        //         if (columnContent.match(typeRegex)) {
-        //             courses[0].majors[j].subjects.push({
-        //                 name: textIndex,
-        //                 subjects: [],
-        //             });
-        //         } else {
-        //             const code = $data('table')
-        //                 .eq(1)
-        //                 .find('tr')
-        //                 .eq(i)
-        //                 .find('td')
-        //                 .eq(1)
-        //                 .text()
-        //                 .trim();
-        //             if (code.match(subjectRegex)) {
-        //                 courses[0].majors[j].subjects
-        //                     .at(-1)
-        //                     .subjects.push(code);
-        //             }
-        //         }
-        //     }
-        // }
-
-
+        //get subjects by type (dai cuong)
+        const tableLength = $data('table').eq(1).find('tr').length;
+        for (let j = 0; j < courses[0].majors.length; j++) {
+            for (let i = 1; i < tableLength; i++) {
+                let textIndex = $data('table')
+                    .eq(1)
+                    .find('tr')
+                    .eq(i)
+                    .find('td')
+                    .eq(0)
+                    .text()
+                    .trim();
+                const columnContent = $data('table')
+                    .eq(1)
+                    .find('tr')
+                    .eq(i)
+                    .find('td')
+                    .eq(0)
+                    .text();
+                if (columnContent.match(typeRegex)) {
+                    courses[0].majors[j].subjects.push({
+                        name: textIndex,
+                        subjects: [],
+                    });
+                } else {
+                    const code = $data('table')
+                        .eq(1)
+                        .find('tr')
+                        .eq(i)
+                        .find('td')
+                        .eq(1)
+                        .text()
+                        .trim();
+                    if (code.match(subjectRegex)) {
+                        courses[0].majors[j].subjects
+                            .at(-1)
+                            .subjects.push(code);
+                    }
+                }
+            }
+        }
 
         //get subjects by orther types
         let typeTitles = '';
         const titleLength = $data('*').filter('h3').length;
         for (let i = 0; i < titleLength; i++) {
+            console.log($data('h3').eq(1).next('table').html());
+
+            const orthertableLength = $data('h3')
+                .eq(i)
+                .next('table')
+                .find('tr').length;
+
             const title = $data('h3').eq(i).text();
+
             if (title) {
                 typeTitles = title.match(regEx)?.at(2).split(':').at(0);
                 courses[0].majors[7].subjects.push({
                     name: typeTitles,
                     subjects: [],
                 });
-                // console.log(typeTitles);
-            }
 
+                for (let j = 1; j < orthertableLength; j++) {
+                    const code = $data('h3')
+                        .eq(i)
+                        .next('table')
+                        .find('tr')
+                        .eq(j)
+                        .find('td')
+                        .eq(1)
+                        .text()
+                        .trim();
+
+                    if (code.match(subjectRegex)) {
+                        courses[0].majors[7].subjects
+                            .at(-1)
+                            .subjects.push(code);
+                    }
+                }
+            }
         }
 
         // const otherType = $data('h3').eq(0).text();
@@ -195,15 +213,6 @@ export class EducationProgramConfiguration implements OnApplicationBootstrap {
 
         console.log('??k?');
 
-        const textDaicuong = $data('table').eq(1).text();
-
-        const subjectIdDaiCuong = Array.from(
-            new Set<string>(
-                Array.from(textDaicuong.matchAll(subjectRegex))
-                    .map((v) => v[0])
-                    .filter((v) => v.length > 4),
-            ).values(),
-        );
         // console.log(subjectIdDaiCuong);
     }
     //once per 6 months
