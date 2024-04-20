5ef8cc3ca46e27551842e8fd426f27a299fb883b
Hoang Hy <hoanghy01122003@gmail.com>
2024-04-20 20:51:52 +0700

    feat(mobile): infinite scroll for news

--- apps/mobile/app/modals/news-feed.tsx
+++ apps/mobile/app/modals/news-feed.tsx
@@ -1,13 +1,25 @@
-import { ScrollView, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, Text, View } from 'react-native';
 import Animated, { FadeInUp, FadeOutUp } from 'react-native-reanimated';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import NewsFeedItem from '../../src/components/NewsFeedItem';
-import { useGeneralNewsFeedQuery } from '../../src/gql/graphql';
+import { NewsFeed, useGeneralNewsFeedLazyQuery } from '../../src/gql/graphql';
 
 export default function NewsFeedModal() {
-    const { data, loading } = useGeneralNewsFeedQuery({
-        variables: { limit: 7, skip: 0 },
-    });
+    const [page, setPage] = useState(0);
+    const [news, setNews] = useState<NewsFeed[]>([]);
+
+    const [refetch, { data, loading }] = useGeneralNewsFeedLazyQuery();
+
+    useEffect(() => {
+        refetch({
+            variables: { limit: 5, skip: 0 },
+            onCompleted(data) {
+                setNews(data.newsFeed);
+                setPage(1);
+            },
+        });
+    }, []);
 
     return (
         <View
@@ -17,27 +29,60 @@ export default function NewsFeedModal() {
             <SafeAreaView style={{ flex: 1 }}>
                 <Text className=" font-semibold text-xl mx-4">News</Text>
                 <View className=" mt-4 flex-1">
-                    <ScrollView className=" flex-1">
-                        <View style={{ paddingVertical: 10 }}>
-                            {loading ? (
-                                <View></View>
-                            ) : (
-                                <View className=" flex flex-col gap-4">
-                                    {data?.newsFeed.map((news, i) => (
-                                        <Animated.View
-                                            key={news.link}
-                                            entering={FadeInUp.delay(
-                                                i * 70 + 100,
-                                            )}
-                                            exiting={FadeOutUp}
-                                        >
-                                            <NewsFeedItem news={news} />
-                                        </Animated.View>
-                                    ))}
-                                </View>
-                            )}
-                        </View>
-                    </ScrollView>
+                    <View className=" flex-1" style={{ paddingVertical: 10 }}>
+                        {loading && news.length === 0 ? (
+                            <View></View>
+                        ) : (
+                            <FlatList
+                                data={news}
+                                keyExtractor={(item) => item.title}
+                                renderItem={({ item, index }) => (
+                                    <Animated.View
+                                        entering={FadeInUp.delay(
+                                            index * 70 + 100,
+                                        )}
+                                        exiting={FadeOutUp}
+                                    >
+                                        <NewsFeedItem news={item} />
+                                    </Animated.View>
+                                )}
+                                refreshing={loading}
+                                onRefresh={() => {
+                                    setNews([]);
+                                    refetch({
+                                        variables: { limit: 5, skip: 0 },
+                                        onCompleted(data) {
+                                            setNews(data.newsFeed);
+                                            setPage(1);
+                                        },
+                                    });
+                                }}
+                                onEndReached={async () => {
+                                    await refetch({
+                                        variables: {
+                                            limit: 5,
+                                            skip: page * 5,
+                                        },
+                                        onCompleted(data) {
+                                            setNews((prev) => [
+                                                ...prev,
+                                                ...data.newsFeed,
+                                            ]);
+                                        },
+                                    });
+                                    setPage(page + 1);
+                                }}
+                                showsVerticalScrollIndicator={true}
+                                onEndReachedThreshold={10}
+                                indicatorStyle="black"
+                                contentContainerStyle={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    gap: 16,
+                                }}
+                            />
+                        )}
+                    </View>
                 </View>
             </SafeAreaView>
         </View>
