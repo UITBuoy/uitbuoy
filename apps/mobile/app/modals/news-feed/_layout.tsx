dd4ef29791b9da2a89714cf7346ff75299c42af8
Hoang Hy <hoanghy01122003@gmail.com>
2024-04-21 22:36:40 +0700

    feat(mobile): update UI of news feed

--- apps/mobile/app/modals/news-feed/_layout.tsx
+++ apps/mobile/app/modals/news-feed/_layout.tsx
@@ -1,7 +1,15 @@
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import { Animated, ScrollView, TouchableOpacity, View } from 'react-native';
+import {
+    Animated,
+    FlatList,
+    ScrollView,
+    TouchableOpacity,
+    View,
+    Text,
+} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import NewsFeed from '../../../src/components/NewsFeed';
+import { useEffect, useRef } from 'react';
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -15,13 +23,14 @@ export default function Layout() {
     ];
 
     return (
-        <SafeAreaView style={{ flex: 1 }}>
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#EFEFEF' }}>
             <View className=" flex-1">
                 <Tab.Navigator
                     tabBar={(props) => <MyTabBar {...props} />}
                     screenOptions={{
                         tabBarLabelStyle: { fontSize: 14 },
                         tabBarScrollEnabled: true,
+                        lazy: true,
                     }}
                 >
                     <Tab.Screen name="General" children={() => <NewsFeed />} />
@@ -41,9 +50,101 @@ export default function Layout() {
 }
 
 function MyTabBar({ state, descriptors, navigation, position }) {
+    const flatListRef = useRef<FlatList>(null);
+
+    useEffect(() => {
+        if (flatListRef.current) {
+            flatListRef.current.scrollToIndex({
+                index: state.index,
+                animated: true,
+                viewPosition: 0.5,
+            });
+        }
+    }, [state.index]);
+
     return (
         <View>
-            <ScrollView horizontal>
+            <View className=" flex flex-row pt-6 pb-3 px-4 rounded-b-2xl">
+                <FlatList
+                    ref={flatListRef}
+                    data={state.routes}
+                    keyExtractor={(item, index) => index.toString()}
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                    renderItem={({ item: route, index }) => {
+                        const { options } = descriptors[route.key];
+                        const label =
+                            options.tabBarLabel !== undefined
+                                ? options.tabBarLabel
+                                : options.title !== undefined
+                                  ? options.title
+                                  : route.name;
+
+                        const isFocused = state.index === index;
+
+                        const onPress = () => {
+                            const event = navigation.emit({
+                                type: 'tabPress',
+                                target: route.key,
+                                canPreventDefault: true,
+                            });
+
+                            if (!isFocused && !event.defaultPrevented) {
+                                navigation.navigate(route.name, route.params);
+                            }
+                        };
+
+                        const onLongPress = () => {
+                            navigation.emit({
+                                type: 'tabLongPress',
+                                target: route.key,
+                            });
+                        };
+
+                        const inputRange = state.routes.map((_, i) => i);
+                        const opacity = position.interpolate({
+                            inputRange,
+                            outputRange: inputRange.map((i) =>
+                                i === index ? 1 : 0.2,
+                            ),
+                        });
+
+                        return (
+                            <TouchableOpacity
+                                key={label}
+                                activeOpacity={0.3}
+                                accessibilityRole="button"
+                                accessibilityState={
+                                    isFocused ? { selected: true } : {}
+                                }
+                                accessibilityLabel={
+                                    options.tabBarAccessibilityLabel
+                                }
+                                testID={options.tabBarTestID}
+                                onPress={onPress}
+                                onLongPress={onLongPress}
+                                style={{
+                                    flex: 1,
+                                    paddingVertical: 8,
+                                    paddingHorizontal: 16,
+                                }}
+                            >
+                                <Animated.Text
+                                    style={{
+                                        textAlign: 'center',
+                                        opacity,
+                                        color: 'black',
+                                        fontWeight: '500',
+                                    }}
+                                >
+                                    {label}
+                                </Animated.Text>
+                            </TouchableOpacity>
+                        );
+                    }}
+                />
+            </View>
+            {/* <ScrollView horizontal>
                 <View className=" flex flex-row pt-6 pb-3 px-4 rounded-b-2xl">
                     {state.routes.map((route, index) => {
                         const { options } = descriptors[route.key];
@@ -118,7 +219,7 @@ function MyTabBar({ state, descriptors, navigation, position }) {
                         );
                     })}
                 </View>
-            </ScrollView>
+            </ScrollView> */}
         </View>
     );
 }
--- apps/mobile/src/components/NewsFeedItem.tsx
+++ apps/mobile/src/components/NewsFeedItem.tsx
@@ -18,33 +18,33 @@ export default function NewsFeedItem({ news }: { news: NewsFeed }) {
                     });
                 }}
             >
-                <View className=" bg-white p-4 px-4 flex flex-col items-start ">
+                <View className=' p-4'>
                     <Text className=" font-bold flex flex-row items-center">
                         {title}
                     </Text>
-                    <View className=" mt-4 flex flex-row gap-2">
-                        <Text className=" px-3 py-1 rounded-lg bg-neutral-95 text-black text-center font-medium text-xs">{`${time} ${type} trước`}</Text>
-                        <Text className=" px-3 py-1 rounded-lg bg-neutral-95 text-black text-center font-medium text-xs">{`${view} lượt xem`}</Text>
-                    </View>
-                    {imageUrl ? (
-                        <Image
-                            source={{ uri: imageUrl }}
-                            style={{
-                                width: '100%',
-                                height: 200,
-                                objectFit: 'cover',
-                            }}
-                            className=" mt-4 rounded-2xl"
-                        />
-                    ) : null}
-                    {description ? (
-                        <Text className=" mt-4 text-neutral-40">
-                            {description}
-                        </Text>
-                    ) : null}
                 </View>
             </NativeButton>
-            <View className=" flex-row p-2 gap-4">
+            <View className=" bg-white px-4 py-2 flex flex-col items-start ">
+                <View className=" flex flex-row gap-2">
+                    <Text className=" px-2 py-1 rounded-lg bg-neutral-95 text-black text-center font-medium text-xs">{`${time} ${type} trước`}</Text>
+                    <Text className=" px-2 py-1 rounded-lg bg-neutral-95 text-black text-center font-medium text-xs">{`${view} lượt xem`}</Text>
+                </View>
+                {imageUrl ? (
+                    <Image
+                        source={{ uri: imageUrl }}
+                        style={{
+                            width: '100%',
+                            height: 200,
+                            objectFit: 'cover',
+                        }}
+                        className=" mt-4 rounded-2xl"
+                    />
+                ) : null}
+                {description ? (
+                    <Text className=" mt-4 text-neutral-40">{description}</Text>
+                ) : null}
+            </View>
+            <View className=" mt-2 flex-row p-2 px-4 gap-4">
                 {tags.map((tag) => (
                     <NativeButton key={tag.name} borderRadius={8}>
                         <View className=" rounded-lg bg-primary-70 px-3 py-1">
