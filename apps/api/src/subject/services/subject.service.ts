c038b3c6ffd003c19dffc24014742f48ff9cb9eb
Zang <bt.hoanggiang@gmail.com>
2024-04-24 04:47:05 +0700

    feat: get all major subject

--- apps/api/src/subject/services/subject.service.ts
+++ apps/api/src/subject/services/subject.service.ts
@@ -1,26 +1,81 @@
-import { Injectable } from '@nestjs/common';
+import { User } from '@/user/entities/user.entity';
+import { Inject, Injectable, forwardRef } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Subject } from '../entities/subject.entity';
+import { ILike, Like, Repository } from 'typeorm';
 import { EducationProgram } from '../entities/educationProgram.entity';
+import { MajorSubject } from '../entities/majorSubject.entity';
+import { Section } from '../entities/section.entity';
+import { Subject } from '../entities/subject.entity';
+import { UserService } from '@/user/services/user.service';
+import { CourseService } from '@/course/services/course.service';
 
 @Injectable()
 export class SubjectService {
     constructor(
-        @InjectRepository(Subject) private subjecRepo: Repository<Subject>,
+        @InjectRepository(Subject) private subjectRepo: Repository<Subject>,
         @InjectRepository(EducationProgram)
         private educationProgramRepo: Repository<EducationProgram>,
+        @InjectRepository(MajorSubject)
+        private majorSubjectRepo: Repository<MajorSubject>,
+        @InjectRepository(Section)
+        private sectionRepo: Repository<Section>,
+        private readonly userService: UserService,
+        @Inject(forwardRef(() => CourseService))
+        private readonly courseService: CourseService,
     ) {}
 
     async findSubjectDataByCode(token: String, code: string) {
-        return this.subjecRepo.findOneBy({ code });
+        return this.subjectRepo.findOneBy({ code });
     }
 
     async findSubjectData(token: String, nameEN: string) {
-        return this.subjecRepo.findBy({ nameEN });
+        return this.subjectRepo.findBy({ nameEN });
     }
 
     async findEducationProgram(token: String, majorName: string) {
         return this.educationProgramRepo.findBy({ majorName });
+    }
+
+    async findEducationProgramById(token: String, id: string) {
+        return this.educationProgramRepo.findOneBy({ id });
+    }
+
+    async findSubjectEducationByYearAndMajorName(
+        year: string,
+        majorName: string,
+    ) {
+        console.log({ year, majorName });
+        return this.educationProgramRepo.findOne({
+            where: {
+                year: ILike(`%20${year}%`),
+                majorName: ILike(`%${majorName}%`),
+            },
+            relations: {
+                sections: {
+                    subjects: true,
+                },
+            },
+        });
+    }
+
+    async findAllSubjectCodeByMajor(user: User, majorName: string) {
+        const majorSubjectCodes = [];
+
+        const year = await this.userService.findYear(user);
+        // const majorName =
+        //     await this.courseService.findUserMajorByCourse(resolverCourses);
+
+        const years = await this.findSubjectEducationByYearAndMajorName(
+            year,
+            majorName,
+        );
+        if (years)
+            for (let i = 0; i < years.sections.length; i++)
+                for (let j = 0; j < years.sections[i].subjects.length; j++) {
+                    majorSubjectCodes.push(years.sections[i].subjects[j].code);
+                }
+
+        console.log({ majorSubjectCodes });
+        return majorSubjectCodes;
     }
 }
