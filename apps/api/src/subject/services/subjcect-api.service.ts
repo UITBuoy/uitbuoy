d09a591e23791a88246280d35a0b5041f6b40ede
Zang <bt.hoanggiang@gmail.com>
2024-04-24 04:48:27 +0700

    refactor: change location

--- apps/api/src/subject/services/subjcect-api.service.ts
+++ apps/api/src/subject/services/subjcect-api.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import WS_FUNCTION from 'src/common/constants/function-name';
-import { SubjectNotFoundException } from 'src/subject/services/errors/not-found.error';
 import { ApiService } from '../../api/services/api.service';
-import { Subject } from '../entities/subject.entity';
 
 @Injectable()
 export class SubjectApiService {
