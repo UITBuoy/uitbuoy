8d320c8e839b807e544b5e1a6ade6588b528e298
Hoang Hy <hoanghy01122003@gmail.com>
2024-05-30 21:08:57 +0700

    feat(api): add current semester of the user

--- apps/api/src/learning-path/resolvers/user-learning-path.resolver.ts
+++ apps/api/src/learning-path/resolvers/user-learning-path.resolver.ts
@@ -1,10 +1,10 @@
 import { CurrentUser } from '@/auth/decorators/current-user.decorator';
 import { JwtAuthGuard } from '@/auth/guards/jwt-auth.guard';
 import { User } from '@/user/entities/user.entity';
-import { UseGuards } from '@nestjs/common';
-import { ResolveField, Resolver } from '@nestjs/graphql';
-import { LearningPathService } from '../learning-path.service';
 import { UserService } from '@/user/services/user.service';
+import { UseGuards } from '@nestjs/common';
+import { Int, ResolveField, Resolver } from '@nestjs/graphql';
+import { LearningPathService } from '../learning-path.service';
 
 @Resolver(() => User)
 export class UserLearningPathResolver {
@@ -27,5 +27,13 @@ export class UserLearningPathResolver {
     @UseGuards(JwtAuthGuard)
     async year(@CurrentUser() user: User): Promise<string> {
         return this.userService.findYear(user);
+    }
+
+    @ResolveField(() => Int, {
+        description: 'Current semester of the user',
+    })
+    @UseGuards(JwtAuthGuard)
+    async semester(@CurrentUser() user: User): Promise<number> {
+        return this.userService.findSemester(user);
     }
 }
